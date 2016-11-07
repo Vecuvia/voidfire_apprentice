c6609d708e05da632c9ce79c7e14e630cc8d21b2
mftrhu <mftrhu+github@inventati.org>
2016-11-07 21:53:51 +0100

    Killed wild tabs

--- data/mobs.js
+++ data/mobs.js
@@ -9,9 +9,9 @@ Mobs.Player = {
 };
 
 Mobs.DummyMob = {
-	position: "DummyRoom",
-	name: "dummy mob",
-	keywords: ["dummy", "mob", "dummy mob"],
-	short_description: "A dummy mob is standing here.",
-	description: "It's just a dummy mob."
+  position: "DummyRoom",
+  name: "dummy mob",
+  keywords: ["dummy", "mob", "dummy mob"],
+  short_description: "A dummy mob is standing here.",
+  description: "It's just a dummy mob."
 };
