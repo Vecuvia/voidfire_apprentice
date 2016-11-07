671b999f333a8bb922b6c8f54363d68f7ee6c1a5
mftrhu <mftrhu+github@inventati.org>
2016-11-07 19:11:23 +0100

    Fix examine command, add goto command

--- data/commands.js
+++ data/commands.js
@@ -13,17 +13,48 @@ Commands.push({
   pattern: "^(x|look\\s+at|examine)\\s+(\\w+)$",
   execute: function (game, captures) {
     var examined = captures[2];
+    var position = Mobs[game.player].position;
     for (var mob in Mobs) {
-      if (Mobs[mob].keywords.includes(examined)) {
+      if (Mobs[mob].position === position && Mobs[mob].keywords.includes(examined)) {
         out(Mobs[mob].description);
         return;
       }
     }
     for (var item in Items) {
-      if (Items[item].keywords.includes(examined)) {
+      if (Items[item].position === position && Items[item].keywords.includes(examined)) {
         out(Items[item].description);
         return;
       }
+    }
+  }
+});
+
+//Goto command
+Commands.push({
+  pattern: "^(go\\s+)?((north|n|south|s|east|e|west|w|up|u|down|d)(?!\\w+)|to\\s+(\\w+)|\\w+)$",
+  execute: function (game, captures) {
+    var cardinal = !captures[4];
+    var direction = captures[4] || captures[3];
+    var position = Mobs[game.player].position;
+    if (cardinal) {
+      if (direction in DirectionAliases) {
+        direction = DirectionAliases[direction];
+      }
+    } else {
+      for (var exit in Rooms[position].exits) {
+        var room = Rooms[position].exits[exit];
+        if (Rooms[room].keywords.includes(direction)) {
+          direction = exit;
+          break;
+        }
+      }
+    }
+    if (direction in Rooms[position].exits) {
+      out("You go " + direction + ".");
+      Mobs[game.player].position = Rooms[position].exits[direction];
+      look(game);
+    } else {
+      out("You can't go in that direction.");
     }
   }
 });
--- data/rooms.js
+++ data/rooms.js
@@ -3,8 +3,19 @@ var Rooms = {};
 Rooms.TowerMageQuarters = {
   name: "Second floor - Master's quarters",
   keywords: ["quarters"],
-  description: "Your master's quarters are quite expansive, with a canopy bed in the middle of the room, an huge wardrobe, and enough candles to rival sunlight. There's a small bathroom to the east and to the north you can access the central staircase.",
-  exits: {}
+  description: "Your master's quarters are quite expansive, with a canopy bed in the middle of the room, an huge wardrobe, and enough lit candles to approximate sunlight. There's a small bathroom to the east and to the north you can access the central staircase.",
+  exits: {
+    east: "TowerMageBathroom"
+  }
+};
+
+Rooms.TowerMageBathroom = {
+  name: "Second floor - Master's bathroom",
+  keywords: ["bathroom"],
+  description: "Your master's bathroom is a large affair, with all the amenities a modern wizard could possibly wish for: toilet, sink, bidet and a large porcelain bathtub. To the west a door leads back to the bedroom.",
+  exits: {
+    west: "TowerMageQuarters"
+  }
 };
 
 Rooms.DummyRoom = {
