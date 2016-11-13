737590f837f74a5df8b189baa233abac75d08ece
mftrhu <mftrhu+github@inventati.org>
2016-11-13 16:39:22 +0100

    Fix pronouns

--- data/misc.js
+++ data/misc.js
@@ -21,30 +21,35 @@ var Pronouns = {
     subject: "you",
     object: "you",
     possessive: "your",
+    possessive_sa: "your",
     reflexive: "yourself"
   },
   it: {
     subject: "it",
     object: "it",
     possessive: "its",
+    possessive_sa: "its",
     reflexive: "itself"
   },
   they: {
     subject: "they",
     object: "them",
-    possessive: "theirs",
+    possessive: "their",
+    possessive_sa: "theirs",
     reflexive: "themselves"
   },
   he: {
     subject: "he",
     object: "him",
     possessive: "his",
+    possessive_sa: "his",
     reflexive: "himself"
   },
   she: {
     subject: "she",
     object: "her",
-    possessive: "hers",
+    possessive: "her",
+    possessive_sa: "hers",
     reflexive: "herself"
   }
 }
--- writing/ideas.md
+++ writing/ideas.md
@@ -7,3 +7,5 @@ Given enough time you can sift through the books in the library to learn new spe
 The spellbook is almost, but not quite, vancian magic. You can, given enough time (read: more than the zombies will give you) cast any spell in it, but if you prepare them ahead of the time by imbuing their magic in your amulet (that you need to carry to quick-cast spells) you can release them in a second.
 
 "Oh, and always look in the mirror before going through it.", he rambles.
+
+Scratch that - the corridor and the hall are both completely empty, except for a golem outside the door that won't let you pass.
